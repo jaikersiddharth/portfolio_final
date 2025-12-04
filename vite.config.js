7d50499c915e975acafc11eaa2eb406c687ac208
jaikersiddharth <jaikersiddharth@email.com>
2025-12-04 20:28:24 +0530

    again fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import {resolve,dirname} from "path";
 import { fileURLToPath } from 'url';
 export default defineConfig({
-  base: '/macOS_portfolio/',
+  base: '/portfolio_final/',
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
